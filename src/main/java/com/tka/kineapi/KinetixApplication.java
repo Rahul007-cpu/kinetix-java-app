f9dd7bc1240703a226f1e06b9edaf5e68a35bd7d
Rahul <rp221272@gmail.com>
2026-01-05 18:38:34 +0530

    Java Backend Ready

---
+++ src/main/java/com/tka/kineapi/KinetixApplication.java
@@ -0,0 +1,13 @@
+package com.tka.kineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinetixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinetixApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tka/kineapi/exceptions/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package com.tka.kineapi.exceptions;
+
+public class DivideByZeroException extends RuntimeException{
+
+	public DivideByZeroException(String msg) {
+		super(msg);
+	}
+}
